4020628a8e949aad110ba20651076597de4fd458
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-07-23 21:01:30 +0300

    fix CommentsController and CommentsService

--- src/main/java/ru/skypro/homework/service/CommentsService.java
+++ src/main/java/ru/skypro/homework/service/CommentsService.java
@@ -3,10 +3,15 @@ package ru.skypro.homework.service;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.Comment;
 import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.entity.AdEntity;
 import ru.skypro.homework.entity.CommentEntity;
+import ru.skypro.homework.entity.UserEntity;
 import ru.skypro.homework.mapper.CommentMapper;
+import ru.skypro.homework.repository.AdEntityRepository;
 import ru.skypro.homework.repository.CommentEntityRepository;
+import ru.skypro.homework.repository.UserEntityRepository;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,22 +19,36 @@ import java.util.Optional;
 public class CommentsService {
     private final CommentEntityRepository repository;
     private final CommentMapper commentMapper;
+    private final UserEntityRepository userEntityRepository;
+    private final AdEntityRepository adEntityRepository;
 
-    public CommentsService(CommentEntityRepository repository, CommentMapper commentMapper) {
+    public CommentsService(CommentEntityRepository repository, CommentMapper commentMapper, UserEntityRepository userEntityRepository, AdEntityRepository adEntityRepository) {
         this.repository = repository;
         this.commentMapper = commentMapper;
+        this.userEntityRepository = userEntityRepository;
+        this.adEntityRepository = adEntityRepository;
     }
 
-    public Comment addComment(Comment comment) {
-        repository.save(commentMapper.toEntity(comment));
+    public Comment addComment(Comment comment, Principal principal, Integer id) {
+        String email  = principal.getName();
+        Optional<UserEntity> optionalUserEntity = userEntityRepository.findByEmail(email);
+        UserEntity userEntity = optionalUserEntity.get();
+        Optional<AdEntity> optionalAdEntity = adEntityRepository.findById(id);
+        AdEntity ad = optionalAdEntity.get();
+        Comment comment1 = new Comment();
+        comment1.setAuthor(userEntity.getId());
+        CommentEntity commentEntity = commentMapper.toEntity(comment);
+        commentEntity.setAdEntity(ad);
+        commentEntity.setUser(userEntity);
+        repository.save(commentEntity);
         return comment;
     }
 
-    public void removeComment(int id) {
-        repository.deleteById(id);
+    public void removeComment( Integer idComment) {
+        repository.deleteById(idComment);
     }
 
-    public Comment updateComment(int id, Comment updateComment) throws Exception{
+    public Comment updateComment(Integer id, Comment updateComment) throws Exception{
         Optional<CommentEntity> entity = repository.findById(id);
         if (entity.isPresent()) {
             CommentEntity newCommentEntity = entity.get();
@@ -48,6 +67,4 @@ public class CommentsService {
         comments.setResults(result);
         return comments;
     }
-
-
 }

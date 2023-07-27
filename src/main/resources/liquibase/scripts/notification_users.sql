eb5fa4e460f665ffd86bbeb92a7a2b8ba208288e
Viking <djviking@yandex.ru>
2023-07-27 18:50:50 +0300

    создание users в базу данных, распределение ролей, тест

---
+++ src/main/resources/liquibase/scripts/notification_users.sql
@@ -0,0 +1,22 @@
+-- — liquibase formatted sql
+-- changeset chernenkov:1
+
+create sequence hibernate_sequence;
+
+-- changeset chernenkov:2
+
+create table users
+(
+    username varchar(255) not null primary key,
+    password varchar(255) not null,
+    enabled  boolean      not null
+);
+
+
+create table authorities
+(
+    username  varchar(255) not null,
+    authority varchar(255) not null,
+    foreign key (username) references users (username),
+    unique (username, authority)
+);

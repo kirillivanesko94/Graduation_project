4e53f10ff9fee6ef7bc7511d030b60f133a5b23b
enovak89 <enovak89@mail.ru>
2023-08-13 21:25:53 +0300

    image controller and imageSerice and avatarService tests + java doc entities and dto's

--- src/main/java/ru/skypro/homework/entity/CommentEntity.java
+++ src/main/java/ru/skypro/homework/entity/CommentEntity.java
@@ -5,17 +5,37 @@ import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+/**
+ * Entity class for comment
+ *
+ * @autor Kirill
+ */
 @Entity
 @Data
 public class CommentEntity {
+    /**
+     * Field primary key
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int pk;
+    /**
+     * Field user
+     */
     @ManyToOne()
     @JoinColumn (name = "id")
     private UserEntity user;
+    /**
+     * Field date of comment creation
+     */
     private LocalDateTime createdAt;
+    /**
+     * Field text of comment
+     */
     private String text;
+    /**
+     * Field advertisement
+     */
     @ManyToOne
     @JoinColumn (name = "adEntity.pk")
     private AdEntity adEntity;
--- src/main/java/ru/skypro/homework/entity/UserEntity.java
+++ src/main/java/ru/skypro/homework/entity/UserEntity.java
@@ -4,64 +4,116 @@ import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.exception.AdNotFoundException;
 
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Entity class for user
+ *
+ * @autor Irina
+ */
 @Entity
 @Data
 public class UserEntity implements UserDetails{
+    /**
+     * Field id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    /**
+     * Field email
+     */
     @Column(unique = true)
     private String email;
+    /**
+     * Field first name
+     */
     private String firstName;
+    /**
+     * Field last name
+     */
     private String lastName;
+    /**
+     * Field phone
+     */
     private String phone;
+    /**
+     * Field password
+     */
     private String password;
+    /**
+     * Field ad
+     */
     @OneToMany(mappedBy = "pk")
     private List<AdEntity> adEntity;
+    /**
+     * Field avatar
+     */
 //    @OneToOne
 //    private ImageEntity image;
     @OneToOne
     private AvatarEntity avatarEntity;
+    /**
+     * Field comments
+     */
     @OneToMany
     private List<CommentEntity> comment;
+    /**
+     * Field role
+     */
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    /**
+     * Service method
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
+    /**
+     * Method to get Password
+     */
     @Override
     public String getPassword(){
         return password;
     }
 
-
+    /**
+     * Method to get email
+     */
     @Override
     public String getUsername() {
         return email;
     }
-
+    /**
+     * Service method
+     */
     @Override
     public boolean isAccountNonExpired() {
         return false;
     }
-
+    /**
+     * Service method
+     */
     @Override
     public boolean isAccountNonLocked() {
         return false;
     }
-
+    /**
+     * Service method
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return false;
     }
-
+    /**
+     * Service method
+     */
     @Override
     public boolean isEnabled() {
         return false;
